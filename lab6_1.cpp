b2debd52d75651b30789789cc735cfdbf985a6c8
ming <mingsa405@gmail.com>
2025-12-09 21:38:31 +0700

    lab6_1.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -2,8 +2,30 @@
 using namespace std;
 
 int main(){
-    cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+    int n[100];
+    int i = 0;
+    int Even = 0;
+    int Odd = 0;
+
+    while (true) {
+        cout << "Enter an integer: ";
+        cin >> n[i];
+
+        if (n[i] == 0) {
+            break;      
+        }
+
+        if (n[i] % 2 == 0) {
+            Even++;
+        } else {
+            Odd++;
+        }
+
+        i++;
+    }
+    
+    cout << "#Even numbers = " << Even << endl;
+    cout << "#Odd numbers = " << Odd;
+
     return 0;
 }
